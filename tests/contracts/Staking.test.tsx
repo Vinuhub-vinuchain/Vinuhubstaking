812967e03d0ca131bd298fb0c17fd7cb935ca659
VINUHUB <aladoemeka1@gmail.com>
2025-11-17 16:01:38 -0800

    Update Staking.test.tsx

--- tests/contracts/Staking.test.tsx
+++ tests/contracts/Staking.test.tsx
@@ -1,24 +1,19 @@
 import { ethers } from 'hardhat';
 import { expect } from 'chai';
 
-describe('Staking', () => {
-  let staking: any, vinToken: any, owner: any, user: any;
+describe('Staking Contract', () => {
+  it('allows staking minimum 10 VIN', async () => {
+    const [owner, user] = await ethers.getSigners();
+    const Token = await ethers.getContractFactory("MockERC20");
+    const token = await Token.deploy();
+    const Staking = await ethers.getContractFactory("Staking");
+    const staking = await Staking.deploy(token.address);
 
-  beforeEach(async () => {
-    [owner, user] = await ethers.getSigners();
-    const VinToken = await ethers.getContractFactory('MockERC20');
-    vinToken = await VinToken.deploy();
-    await vinToken.deployed();
-    const Staking = await ethers.getContractFactory('Staking');
-    staking = await Staking.deploy(vinToken.address);
-    await staking.deployed();
-    await vinToken.mint(user.address, ethers.utils.parseUnits('1000', 18));
-  });
+    await token.mint(user.address, ethers.parseUnits("100", 18));
+    await token.connect(user).approve(staking.address, ethers.parseUnits("10", 18));
+    await staking.connect(user).stake(ethers.parseUnits("10", 18));
 
-  it('allows staking', async () => {
-    await vinToken.connect(user).approve(staking.address, ethers.utils.parseUnits('100', 18));
-    await staking.connect(user).stake(ethers.utils.parseUnits('100', 18));
-    const stakeInfo = await staking.getStakeInfo(user.address);
-    expect(stakeInfo.amount).to.equal(ethers.utils.parseUnits('100', 18));
+    const info = await staking.getStakeInfo(user.address);
+    expect(info.amount).to.equal(ethers.parseUnits("10", 18));
   });
 });
